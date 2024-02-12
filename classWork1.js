26722ca5f16b8b6c7c1a4a1143aeaff8db08000f
vyusal <vyusalb@inbox.ru>
2024-02-12 20:20:31 +0200

    Refactored code

---
+++ classWork1.js
@@ -0,0 +1,44 @@
+function getTimeOfDay(currentHour) {
+    if (currentHour >= 0 && currentHour < 6) {
+        return "ночь";
+    } else if (currentHour >= 6 && currentHour < 12) {
+        return "утро";
+    } else if (currentHour >= 12 && currentHour < 18) {
+        return "день";
+    } else {
+        return "вечер";
+    }
+}
+console.log(getTimeOfDay(7));
+for (let i = 1; i <= 10; i++){
+    console.log(i)
+}
+for (let i = 1; i <= 10; i++){
+    if (i === 7 ){
+        break;
+    }
+    console.log(i)
+}
+let number = 1;
+while (number <= 20) {
+    if (number % 2 === 0 && number % 5 !== 0) {
+        console.log(number);
+    }
+    number++;
+}
+
+function formatPhoneNumber(phoneNumber) {
+    if (!phoneNumber.includes("123")) {
+        phoneNumber = "(123) " + phoneNumber;
+        return phoneNumber
+    }else {
+        const areaCode = phoneNumber.substring(0, 3);
+        const firstPart = phoneNumber.substring(3, 6);
+        const secondPart = phoneNumber.substring(6);
+
+        return `(${areaCode}) ${firstPart}-${secondPart}`;
+    }
+}
+
+// Пример использования:
+console.log(formatPhoneNumber("1234567890"))
