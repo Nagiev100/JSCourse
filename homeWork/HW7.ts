4f5af67202f635c7ceb105dd1ccaee0fa8979934
vyusal <vyusalb@inbox.ru>
2024-02-28 23:26:06 +0200

    HW7 extra task

--- homeWork/HW7.ts
+++ homeWork/HW7.ts
@@ -98,6 +98,22 @@ a3({f1: "malaka"});
 const _a4: number = a4();
 const _a5: string = a5(...["lalaka", "malaka"]);
 
+//6 Для данного кортежа вам нужно создать универсальную функцию для определения его длины.
+// Например:
+//
+// type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+// type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+//
+// type teslaLength = Length<tesla>  // expected 4
+// type spaceXLength = Length<spaceX> // expected 5
+type Length<T extends any[]> = T['length'];
+
+type tesla = ['tesla', 'model 3', 'model X', 'model Y'];
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'];
+
+type teslaLength = Length<tesla>;
+type spaceXLength = Length<spaceX>;
+
 
 
 
