91d9e98148939a0da0a837010d6d217d58ef2287
vyusal <vyusalb@inbox.ru>
2024-03-18 20:31:09 +0200

    Modul html

--- html/homeWork/2/script.js
+++ html/homeWork/2/script.js
@@ -37,5 +37,69 @@ document.getElementById('burger').addEventListener('click', function (){
 
 
 
+const book = [{
+    winter:[{
+        oneBook:{
+            nameAuthor:'Staff Picks',
+            genre:'The Book Eaters By Sunyi Dean',
+            description:'An unusual sci-fi story about a book eater woman who tries desperately to save her dangerous mind-eater son from tradition and certain death. Complete with dysfunctional family values, light Sapphic romance, and a strong, complex protagonist. Not for the faint of heart.'
+        },
+        TwoBook:{
+            nameAuthor:'Staff Picks',
+            genre:'Cackle By Rachel Harrison',
+            description:'Are your Halloween movies of choice The Witches of Eastwick and Practical Magic? Look no further than here - where a woman recovering from a breakup moves to a quaint town in upstate New York and befriends a beautiful witch.'
+        },
+        ThreeBook:{
+            nameAuthor:'Staff Picks',
+            genre:'Dante: Poet of the Secular World By Erich Auerbach',
+            description:'Auerbach s engaging book places the \'Comedy\' within the tradition of epic, tragedy, and philosophy in general, arguing for Dante\'s uniqueness as one who raised the individual and his drama of soul into something of divine significance—an inspired introduction to Dante\'s main themes.'
+        },
+        FourBook:{
+            nameAuthor:'Staff Picks',
+            genre:'The Last Queen By Clive Irving',
+            description:'A timely and revelatory new biography of Queen Elizabeth (and her family) exploring how the Windsors have evolved and thrived as the modern world has changed around them.'
+        },
+
+    }]
+
+}]
+
+const winterRadio = document.getElementById('winter');
+const springRadio = document.getElementById('spring');
+const summerRadio = document.getElementById('summer');
+const autumnRadio = document.getElementById('autumn');
+
+const winterContent = document.getElementById('winterContent');
+const springContent = document.getElementById('springContent');
+const summerContent = document.getElementById('summerContent');
+const autumnContent = document.getElementById('autumnContent');
+
+winterRadio.addEventListener('click', function() {
+    winterContent.style.display = 'grid';
+    springContent.style.display = 'none';
+    summerContent.style.display = 'none';
+    autumnContent.style.display = 'none';
+});
+
+springRadio.addEventListener('click', function() {
+    winterContent.style.display = 'none';
+    springContent.style.display = 'grid';
+    summerContent.style.display = 'none';
+    autumnContent.style.display = 'none';
+});
+
+summerRadio.addEventListener('click', function() {
+    winterContent.style.display = 'none';
+    springContent.style.display = 'none';
+    summerContent.style.display = 'grid';
+    autumnContent.style.display = 'none';
+});
+
+autumnRadio.addEventListener('click', function() {
+    winterContent.style.display = 'none';
+    springContent.style.display = 'none';
+    summerContent.style.display = 'none';
+    autumnContent.style.display = 'grid';
+});
 
 
