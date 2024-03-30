6c7e4da49f9e503b3b5c2e504c238d20521cbb4b
vyusal <vyusalb@inbox.ru>
2024-03-30 14:55:12 +0200

    Remove react and js folders

--- React/react-course/src/helpers/style/colors.ts
+++
@@ -1,5 +0,0 @@
-export enum COLORS {
-    Red = '#FF768E',
-    White = '#F6F6F6',
-    Blue =  '#344472'
-}
--- React/react-course/src/pages/notFoundPage/NotFoundPage.tsx
+++
@@ -1,9 +0,0 @@
-import {FC} from "react";
-
-export const NotFoundPage: FC = () =>{
-    return(
-        <>
-            <p>not found</p>
-        </>
-    )
-}
